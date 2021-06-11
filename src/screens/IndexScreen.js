3275b9bc9cc1fe6f269d45512788c1723a33e101
Ayush Yadav <aayush.sang@gmail.com>
2021-06-11 13:18:23 +0530

    Add Index Screen

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const IndexScreen = () => {
+    return (
+        <View>
+            <Text>Yo</Text>
+        </View>
+    )
+}
+
+export default IndexScreen
+
+const styles = StyleSheet.create({})
