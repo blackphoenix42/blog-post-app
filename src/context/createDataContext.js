c7624130e219b753b534a61d378eeb852a233747
Ayush Yadav <aayush.sang@gmail.com>
2021-06-17 12:14:00 +0530

    Refactor

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,1 +1,17 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+export default (reducer, actions, initialstate) => {
+    const Context = React.createContext()
+
+    const Provider = ({ children }) => {
+        const [state, dispatch] = useReducer(reducer, initialstate)
+        const boundActions = {}
+        for (let key in actions) {
+            boundActions[key] = actions[key](dispatch)
+        }
+        return <Context.Provider value={{ state, ...boundActions }}>
+            {children}
+        </Context.Provider >
+    }
+    return { Context, Provider }
+}
